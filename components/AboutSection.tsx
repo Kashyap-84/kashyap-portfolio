7f7a53138359d17e6dbd0c8764a248736c7a3bc8
Kashyap Kaligotla <kashyap@Veeras-MacBook-Air.local>
2026-05-01 00:52:33 -0500

    Updated About Section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Image from "next/image"
 
 const skills = [
   { skill: "Python" },
@@ -87,13 +86,6 @@ const AboutSection = () => {
                 )
               })}
             </div>
-            <Image
-              src="/hero-image.png"
-              alt=""
-              width={325}
-              height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
-            />
           </div>
         </div>
       </div>
